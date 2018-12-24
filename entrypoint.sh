07ac16782abec616d82036e47a94e21421ea6515
Julián Maestri <serpi90@gmail.com>
2018-12-24 10:57:52 -0300

    fix: shellcheck warnings

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -eu
 # set -e error handler.
 on_error() {
     echo >&2 "Error on line ${1}${3+: ${3}}; RET ${2}."
-    exit $2
+    exit "$2"
 }
 trap 'on_error ${LINENO} $?' ERR 2>/dev/null || true # some shells don't have ERR trap.
 
